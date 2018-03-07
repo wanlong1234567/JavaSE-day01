153d96f3b7e31e9374af67fcb9702eb149e3aaa0
wanlong1234567 <2531165965@qq.com>
2018-03-07 17:17:59 +0800

    关于String的trim的方法

---
+++ day01_all/day01/String_trim.java
@@ -0,0 +1,16 @@
+package day01;
+/**
+ * String trim()
+ * È¥³ýµ±Ç°×Ö·û´®Á½±ßµÄ¿Õ°××Ö·û
+ * @author adminitartor
+ *
+ */
+public class String_trim {
+	public static void main(String[] args) {
+		String str = "  hello		";
+		System.out.println(str);
+		
+		String trim = str.trim();
+		System.out.println(trim);
+	}
+}
