ff574ef80229185305aac08121d305d63d52e633
wanlong1234567 <2531165965@qq.com>
2018-03-07 17:17:46 +0800

    关于String的toLowCase和toUpperCase的方法的方法

---
+++ day01_all/day01/String_toLowerCase_toUpperCase.java
@@ -0,0 +1,27 @@
+package day01;
+/**
+ * String toLowerCase()
+ * String toUpperCase()
+ * 将当前在字符串中的英文部分转换为全大写和全小写
+ * @author adminitartor
+ *
+ */
+public class String_toLowerCase_toUpperCase {
+	public static void main(String[] args) {
+		String str = "我爱Java";
+		
+		String upper = str.toUpperCase();
+		System.out.println(upper);
+		
+		String lower = str.toLowerCase();
+		System.out.println(lower);
+	}
+}
+
+
+
+
+
+
+
+
