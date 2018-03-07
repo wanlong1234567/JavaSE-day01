6d55b32ee8e0efa319beb58cc7c19620aa00b500
wanlong1234567 <2531165965@qq.com>
2018-03-07 17:16:24 +0800

    关于String的length的方法

---
+++ day01_all/day01/String_length.java
@@ -0,0 +1,17 @@
+package day01;
+/**
+ * int length()
+ * 返回当前字符串的长度,字符个数
+ * @author adminitartor
+ *
+ */
+public class String_length {
+	public static void main(String[] args) {
+		String str = "我爱java";
+		System.out.println(str.length());
+	}
+}
+
+
+
+
